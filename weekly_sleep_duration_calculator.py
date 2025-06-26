e2920ac5f89e3cd635249e3aff0d6d114a1a8946
Shawn Cheng <68705406+shawnchengdev@users.noreply.github.com>
2025-06-25 17:59:46 -0700

    Update weekly_sleep_duration_calculator.py

--- weekly_sleep_duration_calculator.py
+++ weekly_sleep_duration_calculator.py
@@ -4,7 +4,8 @@ import datetime
 # Takes a dict with specific date in a week and calculates the number of seconds remaining before reaching that specific date
 def get_sleep_duration(schedule:dict):
     current_date = datetime.datetime.now()
-    # Sets to January first, since the real date isn"t necessary and it will allow us to disregard month sizes
+
+    # Sets to January first, since the real date isn't necessary, and it will allow us to disregard month sizes
     current_date = current_date.replace(year = 1, month = 1, day = 1, microsecond = 0)     
     scheduled_date = datetime.datetime(year = 1, month = 1, day = 1, hour = schedule["Hour"], minute = schedule["Minute"], second = schedule["Second"])
 
@@ -18,8 +19,10 @@ def get_sleep_duration(schedule:dict):
 
     difference = scheduled_date - current_date
     sleep_seconds = difference.total_seconds()
+
+    # If it's the scheduled time, sleep_seconds will be set to 7 days
     if (sleep_seconds == 0):
         sleep_seconds = 604800
-        
+
     print("Proceeding to sleep for", int(sleep_seconds/86400), "days,", int(sleep_seconds/3600) % 24, "hours,", int(sleep_seconds/60) % 60, "minutes and", sleep_seconds % 60, "seconds")
-    return (sleep_seconds)
+    return sleep_seconds
